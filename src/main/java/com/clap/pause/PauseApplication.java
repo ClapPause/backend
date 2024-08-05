0ec64ea4bc91f1a15eb74c0808c806c4763b583a
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-08-05 17:51:56 +0900

    initial: 클랩 백엔드 서버 초기 세팅

---
+++ src/main/java/com/clap/pause/PauseApplication.java
@@ -0,0 +1,13 @@
+package com.clap.pause;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PauseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PauseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clap/pause/PauseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clap.pause;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PauseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
