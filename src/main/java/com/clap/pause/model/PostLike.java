108de4025ef88461d434844892fdcd02a908a4b3
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-08-06 14:56:33 +0900

    add: 글 좋아요/스크랩 매핑하기
    좋아요, 스크랩 매핑하기

---
+++ src/main/java/com/clap/pause/model/PostLike.java
@@ -0,0 +1,37 @@
+package com.clap.pause.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table(name = "post_like")
+@SQLDelete(sql = "update post_like set deleted = true where id = ?")
+@SQLRestriction("deleted is false")
+public class PostLike extends BaseEntity {
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", referencedColumnName = "id")
+    private Post post;
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", referencedColumnName = "id")
+    private Member member;
+    @NotNull
+    @Column(name = "deleted")
+    private Boolean deleted = Boolean.FALSE;
+
+    protected PostLike() {
+    }
+
+    public PostLike(Post post, Member member) {
+        this.post = post;
+        this.member = member;
+    }
+}
