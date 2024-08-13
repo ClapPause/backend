1b6f730781a01568704a77f04a2f57b898aafab8
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-08-13 23:09:27 +0900

    test: 회원가입 시도하는 테스트 코드 작성
    테스트코드 작성

--- src/test/java/com/clap/pause/service/auth/AuthServiceTest.java
+++ src/test/java/com/clap/pause/service/auth/AuthServiceTest.java
@@ -1,8 +1,17 @@
 package com.clap.pause.service.auth;
 
+import com.clap.pause.dto.auth.RegisterRequest;
+import com.clap.pause.exception.DuplicatedEmailException;
+import com.clap.pause.model.Gender;
+import com.clap.pause.service.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 @Transactional
@@ -10,4 +19,21 @@ class AuthServiceTest {
 
     @Autowired
     private AuthService authService;
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Test
+    @DisplayName("회원가입 시도하기")
+    void register_success() {
+        //given
+        var registerRequest = new RegisterRequest("테스트", "test@naver.com", "testPassword", LocalDate.of(1999, 1, 16), Gender.MALE, "직업");
+        var auth = authService.register(registerRequest);
+        //when, then
+        Assertions.assertThatThrownBy(() -> authService.register(registerRequest)).isInstanceOf(DuplicatedEmailException.class);
+
+        var memberId = jwtProvider.getMemberIdWithToken(auth.token());
+        memberService.deleteMember(memberId);
+    }
 }
