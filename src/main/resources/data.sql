ade66a4f9ebfc01daf43006faf9aebd68ca336b6
boyun <bu339@naver.com>
2024-08-19 17:53:04 +0900

    add: data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into department_group(name, deleted)
+values ('IT', false);
+
+insert into member(name, email, password, phone_number, birth,
+                   gender, job, profile_imgae, member_role, certified, deleted)
+values ('테스트1', 'test@naver.com', '1234', '010-1234-1234', 2000 - 00 - 00,
+        'MALE', 'student', image.jpg, 'MEMBER', false, false);
