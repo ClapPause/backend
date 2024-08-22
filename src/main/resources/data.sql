c937bbf3430c23a7a5460ecf0f15461966dc7e03
boyun <bu339@naver.com>
2024-08-22 14:46:52 +0900

    add : data.sql에 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
 insert into department_group(name, deleted)
 values ('IT', false);
 
+insert into university_department(department_group_id, university, department, deleted)
+values (1, '충남대학교', '컴퓨터공학과', false);
+
 
