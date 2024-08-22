a3a6f5f772136871af00b5dad260c4922b5ade67
boyun <bu339@naver.com>
2024-08-22 15:24:40 +0900

    fix : departmentGroupId 필드 추가

--- src/main/java/com/clap/pause/dto/post/response/PostResponse.java
+++ src/main/java/com/clap/pause/dto/post/response/PostResponse.java
@@ -2,7 +2,6 @@ package com.clap.pause.dto.post.response;
 
 import com.clap.pause.model.PostCategory;
 import com.clap.pause.model.PostType;
-
 import java.time.LocalDateTime;
 
 public record PostResponse(
@@ -11,9 +10,11 @@ public record PostResponse(
         String contents,
         PostCategory postCategory,
         PostType postType,
+        Long departmentGroupId,
         LocalDateTime createdAt
 ) {
-    public static PostResponse of(Long id, String title, String contents, PostCategory postCategory, PostType postType, LocalDateTime createdAt) {
-        return new PostResponse(id, title, contents, postCategory, postType, createdAt);
+    public static PostResponse of(Long id, String title, String contents, PostCategory postCategory, PostType postType,
+                                  Long departmentGroupId, LocalDateTime createdAt) {
+        return new PostResponse(id, title, contents, postCategory, postType, departmentGroupId, createdAt);
     }
 }
