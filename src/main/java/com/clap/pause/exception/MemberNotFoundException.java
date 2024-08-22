92f1633f45225eb1ad53aa5437e62225f4e0f46b
boyun <bu339@naver.com>
2024-08-22 18:55:54 +0900

    feat : 멤버가져오는 메소드

---
+++ src/main/java/com/clap/pause/exception/MemberNotFoundException.java
@@ -0,0 +1,8 @@
+package com.clap.pause.exception;
+
+public class MemberNotFoundException extends RuntimeException {
+
+    public MemberNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/clap/pause/service/MemberService.java
+++ src/main/java/com/clap/pause/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.clap.pause.service;
 
+import com.clap.pause.exception.MemberNotFoundException;
 import com.clap.pause.exception.NotFoundElementException;
+import com.clap.pause.model.Member;
 import com.clap.pause.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,5 +25,17 @@ public class MemberService {
             throw new NotFoundElementException("존재하지 않는 이용자의 ID 입니다.");
         }
         memberRepository.deleteById(memberId);
+    }
+
+    /**
+     * 멤버 id로 멤버 찾는 메소드
+     *
+     * @param memberId
+     * @return member
+     */
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(
+                () -> new MemberNotFoundException("해당 멤버가 존재하지 않습니다.")
+        );
     }
 }
