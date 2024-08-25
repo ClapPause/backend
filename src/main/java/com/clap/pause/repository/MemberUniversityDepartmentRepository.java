1fc014b62be1149ea4aa20866070f091c2031c5f
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-08-25 19:56:07 +0900

    refactor: 레포지토리 어노테이션 중복 제거
    레포지토리 어노테이션 중복 제거

--- src/main/java/com/clap/pause/repository/MemberUniversityDepartmentRepository.java
+++ src/main/java/com/clap/pause/repository/MemberUniversityDepartmentRepository.java
@@ -2,11 +2,9 @@ package com.clap.pause.repository;
 
 import com.clap.pause.model.MemberUniversityDepartment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface MemberUniversityDepartmentRepository extends JpaRepository<MemberUniversityDepartment, Long> {
     List<MemberUniversityDepartment> findAllByMemberId(Long memberId);
 }
--- src/main/java/com/clap/pause/repository/UniversityDepartmentRepository.java
+++ src/main/java/com/clap/pause/repository/UniversityDepartmentRepository.java
@@ -2,11 +2,9 @@ package com.clap.pause.repository;
 
 import com.clap.pause.model.UniversityDepartment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface UniversityDepartmentRepository extends JpaRepository<UniversityDepartment, Long> {
     List<UniversityDepartment> findAllByUniversity(String university);
 }
