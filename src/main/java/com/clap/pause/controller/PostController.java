b245b35916013ddca0ac7722d45ea730036fc352
boyun <bu339@naver.com>
2024-08-26 16:41:01 +0900

    fix : post 생성과 조회 수정

--- src/main/java/com/clap/pause/controller/PostController.java
+++ src/main/java/com/clap/pause/controller/PostController.java
@@ -1,13 +1,21 @@
 package com.clap.pause.controller;
 
 import com.clap.pause.dto.post.request.PostRequest;
+import com.clap.pause.dto.post.response.PostListResponse;
 import com.clap.pause.dto.post.response.PostResponse;
+import com.clap.pause.exception.PostAccessException;
+import com.clap.pause.model.Post;
+import com.clap.pause.service.MemberUniversityDepartmentService;
 import com.clap.pause.service.PostService;
 import jakarta.validation.Valid;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+    private final MemberUniversityDepartmentService memberUniversityDepartmentService;
 
     /**
      * 기본 게시글을 생성
@@ -27,9 +36,11 @@ public class PostController {
      * @return postResponse
      */
     @PostMapping
-    public ResponseEntity<PostResponse> saveDefaultPost(@Valid @RequestBody PostRequest postRequest) {
+    public ResponseEntity<PostResponse> saveDefaultPost(
+            @PathVariable(name = "departmentgroupId") Long departmentgroupId,
+            @Valid @RequestBody PostRequest postRequest) {
         var memberId = getMemberId();
-        var post = postService.saveDefaultPost(memberId, postRequest);
+        var post = postService.saveDefaultPost(memberId, postRequest, departmentgroupId);
         return ResponseEntity.created(URI.create("/api/departmentgroups/" + post.departmentGroupId()
                         + "/posts" + post.id()))
                 .body(post);
@@ -41,15 +52,19 @@ public class PostController {
      * @param departmentGroupId
      * @return List<PostListResponse>
      */
-//    @GetMapping
-//    public ResponseEntity<List<PostListResponse>> getAllPosts(Long departmentGroupId) {
-////        var posts = postService.getAllPosts(departmentGroupId);
-////        return ResponseEntity.ok().body(posts);
-//    }
-
-
-//    @PostMapping
-//    public ResponseEntity<PostResponse> updatePost()
+    @GetMapping
+    public ResponseEntity<List<PostListResponse>> getAllPosts(
+            @PathVariable(name = "departmentgroupId") Long departmentGroupId) throws PostAccessException {
+        var postList = postService.getAllPosts(departmentGroupId);
+        List<PostListResponse> postResponses = new ArrayList<>();
+        for (Post post : postList) {
+            var memberUniversityDepartmentResponses =
+                    memberUniversityDepartmentService.getMemberUniversityDepartments(post.getMember().getId());
+            var response = postService.getMemberInfo(post, memberUniversityDepartmentResponses);
+            postResponses.add(response);
+        }
+        return ResponseEntity.ok().body(postResponses);
+    }
 
     /**
      * Header의 Authorization 으로 memberId를 return함
--- src/main/java/com/clap/pause/service/PostService.java
+++ src/main/java/com/clap/pause/service/PostService.java
@@ -1,12 +1,17 @@
 package com.clap.pause.service;
 
+import com.clap.pause.dto.memberUniversityDepartment.MemberUniversityDepartmentResponse;
 import com.clap.pause.dto.post.request.PostRequest;
+import com.clap.pause.dto.post.response.PostListResponse;
 import com.clap.pause.dto.post.response.PostResponse;
 import com.clap.pause.exception.NotFoundElementException;
+import com.clap.pause.exception.PostAccessException;
 import com.clap.pause.model.Post;
 import com.clap.pause.repository.DepartmentGroupRepository;
 import com.clap.pause.repository.MemberRepository;
+import com.clap.pause.repository.MemberUniversityDepartmentRepository;
 import com.clap.pause.repository.post.PostRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +24,7 @@ public class PostService {
     private final PostRepository postRepository;
     private final MemberRepository memberRepository;
     private final DepartmentGroupRepository departmentGroupRepository;
+    private final MemberUniversityDepartmentRepository memberUniversityDepartmentRepository;
 
     /**
      * 게시글 저장하는 메소드
@@ -27,8 +33,8 @@ public class PostService {
      * @param postRequest
      * @return postResponse
      */
-    public PostResponse saveDefaultPost(Long memberId, PostRequest postRequest) {
-        var post = savePostWithPostRequest(memberId, postRequest);
+    public PostResponse saveDefaultPost(Long memberId, PostRequest postRequest, Long departmentGroupId) {
+        var post = savePostWithPostRequest(memberId, postRequest, departmentGroupId);
         return getPostResponse(post);
     }
 
@@ -39,10 +45,10 @@ public class PostService {
      * @param postRequest
      * @return
      */
-    private Post savePostWithPostRequest(Long memberId, PostRequest postRequest) {
+    private Post savePostWithPostRequest(Long memberId, PostRequest postRequest, Long departmentGroupId) {
         var member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new NotFoundElementException("존재하지 않는 이용자입니다."));
-        var departmentGroup = departmentGroupRepository.findById(postRequest.departmentGroupId())
+        var departmentGroup = departmentGroupRepository.findById(departmentGroupId)
                 .orElseThrow(() -> new NotFoundElementException("존재하지 않는 학과그룹입니다."));
         var post = new Post(member, departmentGroup, postRequest.title(), postRequest.contents(),
                 postRequest.postCategory(), postRequest.postType());
@@ -66,16 +72,28 @@ public class PostService {
      * @param departmentGroupId
      * @return postListResponse
      */
-//    public List<PostListResponse> getAllPosts(Long departmentGroupId) {
-//        //departmentId로 Post,Member,UniversityDepartment등의 정보를 얻음
-//
-//        var postList = postRepository.getPostListsByDepartmentGroup(departmentGroupId);
-//        for(Post post: postList){
-//
-//
-//        }
-//        return
-//    }
+    public List<Post> getAllPosts(Long departmentGroupId) {
+//        departmentId로 departmentGroup 조회
+        var departmentGroup = departmentGroupRepository.findById(departmentGroupId)
+                .orElseThrow(() -> new NotFoundElementException("학과 그룹이 존재하지 않습니다."));
+        return postRepository.findByDepartmentGroupOrderByCreatedAtDesc(departmentGroup);
+    }
 
+    public PostListResponse getMemberInfo(Post post, List<MemberUniversityDepartmentResponse> responseList)
+            throws PostAccessException {
+        for (MemberUniversityDepartmentResponse response : responseList) {
+            //여러개의 전공 중 현재 게시판의 departmentGroup과 일치하면 PostListResponse를 생성
+            if (response.departmentGroupResponse().id().equals(post.getDepartmentGroup().getId())) {
+                return getPostListResponse(post, response);
+            }
+        }
+        // 일치하는 전공이 없다면 예외를 던짐
+        throw new PostAccessException("회원이 해당 학과그룹에 권한이 없어 조회가 불가 합니다.");
+    }
 
+    private PostListResponse getPostListResponse(Post post, MemberUniversityDepartmentResponse response) {
+        return PostListResponse.of(post.getId(), post.getTitle(), post.getContents(), post.getPostCategory(),
+                post.getPostType(), post.getCreatedAt(), post.getMember().getName(), response.university(),
+                response.department());
+    }
 }
