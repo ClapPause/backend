09d31d3027b3f13130c544f7741629ab64e63e6f
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-09-01 18:04:07 +0900

    test: AuthService 계층 테스트코드 단위테스트로 전환 작업 수행
    AuthService 계층 테스트코드 단위테스트로 전환

--- src/test/java/com/clap/pause/service/auth/AuthServiceTest.java
+++ src/test/java/com/clap/pause/service/auth/AuthServiceTest.java
@@ -8,24 +8,23 @@ import com.clap.pause.exception.NotFoundElementException;
 import com.clap.pause.model.Gender;
 import com.clap.pause.model.Member;
 import com.clap.pause.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class AuthServiceTest {
 
     @Mock
@@ -36,6 +35,7 @@ class AuthServiceTest {
 
     @InjectMocks
     private AuthService authService;
+
     @Spy
     private BCryptPasswordEncoder passwordEncoder;
 
@@ -55,69 +55,78 @@ class AuthServiceTest {
         //when
         var auth = authService.register(registerRequest);
         // then
-        assertThat(auth).isNotNull();
-        assertThat(auth.token()).isEqualTo("mockedJwtToken");
+        Assertions.assertThat(auth.token())
+                .isEqualTo("mockedJwtToken");
     }
 
     @Test
     @DisplayName("중복된 이메일로 회원가입을 요청하면 실패한다")
     void register_fail_duplicatedEmail() {
         //given
-        var registerRequest = new RegisterRequest("테스트", "test@naver.com", "testPassword", LocalDate.of(1999, 1, 16), Gender.MALE, "직업", "010-1234-1234");
-        var auth = authService.register(registerRequest);
-        //when, then
-        assertThatThrownBy(() -> authService.register(registerRequest)).isInstanceOf(DuplicatedEmailException.class);
+        var registerRequest = getRegisterRequest();
 
-        var memberId = jwtProvider.getMemberIdWithToken(auth.token());
-        memberService.deleteMember(memberId);
+        when(memberRepository.existsByEmail(any(String.class)))
+                .thenReturn(true);
+        //when, then
+        Assertions.assertThatThrownBy(() -> authService.register(registerRequest))
+                .isInstanceOf(DuplicatedEmailException.class);
     }
 
     @Test
     @DisplayName("안내에 맞게 로그인을 요청하면 성공한다")
     void login_success() {
         //given
-        var registerRequest = new RegisterRequest("테스트", "test@naver.com", "testPassword", LocalDate.of(1999, 1, 16), Gender.MALE, "직업", "010-1234-1234");
-        authService.register(registerRequest);
-        var loginRequest = new LoginRequest("test@naver.com", "testPassword");
+        var loginRequest = getLoginRequest();
+        var member = getSavedMember();
+
+        when(memberRepository.findByEmail(any(String.class)))
+                .thenReturn(Optional.of(member));
+        when(jwtProvider.generateToken(any(Member.class)))
+                .thenReturn("mockedJwtToken");
         //when
         var auth = authService.login(loginRequest);
         //then
-        assertThat(auth).isNotNull();
-
-        var memberId = jwtProvider.getMemberIdWithToken(auth.token());
-        memberService.deleteMember(memberId);
+        Assertions.assertThat(auth.token())
+                .isEqualTo("mockedJwtToken");
     }
 
     @Test
     @DisplayName("존재하지 않는 이메일로 로그인을 요청하면 실패한다")
     void login_fail_noExistEmail() {
         //given
-        var registerRequest = new RegisterRequest("테스트", "test@naver.com", "testPassword", LocalDate.of(1999, 1, 16), Gender.MALE, "직업", "010-1234-1234");
-        var auth = authService.register(registerRequest);
-        var loginRequest = new LoginRequest("test1@naver.com", "testPassword");
-        //when, then
-        assertThatThrownBy(() -> authService.login(loginRequest)).isInstanceOf(NotFoundElementException.class);
+        var loginRequest = getLoginRequest();
 
-        var memberId = jwtProvider.getMemberIdWithToken(auth.token());
-        memberService.deleteMember(memberId);
+        when(memberRepository.findByEmail(any(String.class)))
+                .thenThrow(new NotFoundElementException("존재하지 않는 이메일입니다."));
+        //when, then
+        Assertions.assertThatThrownBy(() -> authService.login(loginRequest))
+                .isInstanceOf(NotFoundElementException.class);
     }
 
     @Test
     @DisplayName("잘못된 패스워드로 로그인을 요청하면 실패한다")
     void login_fail_invalidPassword() {
         //given
-        var registerRequest = new RegisterRequest("테스트", "test@naver.com", "testPassword", LocalDate.of(1999, 1, 16), Gender.MALE, "직업", "010-1234-1234");
-        var auth = authService.register(registerRequest);
-        var loginRequest = new LoginRequest("test@naver.com", "testPassword2");
-        //when, then
-        assertThatThrownBy(() -> authService.login(loginRequest)).isInstanceOf(InvalidLoginInfoException.class);
+        var loginRequest = getErrorLoginRequest();
+        var member = getSavedMember();
 
-        var memberId = jwtProvider.getMemberIdWithToken(auth.token());
-        memberService.deleteMember(memberId);
+        when(memberRepository.findByEmail(any(String.class)))
+                .thenReturn(Optional.of(member));
+        //when, then
+        Assertions.assertThatThrownBy(() -> authService.login(loginRequest))
+                .isInstanceOf(InvalidLoginInfoException.class);
     }
 
     private RegisterRequest getRegisterRequest() {
         return new RegisterRequest("테스트", "test@naver.com", "testPassword", LocalDate.of(1999, 1, 16), Gender.MALE, "직업", "010-1234-1234");
+    }
+
+    private LoginRequest getLoginRequest() {
+        return new LoginRequest("test@naver.com", "testPassword");
+    }
+
+    private LoginRequest getErrorLoginRequest() {
+        return new LoginRequest("test@naver.com", "errorPassword");
     }
 
     private Member getSavedMember() {
