f5fac84fe76f7dab286c30af217da1416924a634
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-09-04 20:59:18 +0900

    test: 학과그룹 Service 계층에 대한 테스트 작업 수행
    테스트 작업 수행

--- src/test/java/com/clap/pause/service/DepartmentGroupServiceTest.java
+++ src/test/java/com/clap/pause/service/DepartmentGroupServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -103,6 +104,59 @@ class DepartmentGroupServiceTest {
         //when, then
         Assertions.assertThatThrownBy(() -> departmentGroupService.updateDepartmentGroup(1L, departmentGroupRequest))
                 .isInstanceOf(DuplicatedException.class);
+    }
+
+    @Test
+    @DisplayName("존재하는 학과 그룹을 ID로 조회하면 성공한다.")
+    void getDepartmentGroup_success() {
+        //given
+        var departmentGroup = new DepartmentGroup("테스트");
+
+        when(departmentGroupRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(departmentGroup));
+        //when
+        var foundedDepartmentGroup = departmentGroupService.getDepartmentGroup(1L);
+        //then
+        Assertions.assertThat(foundedDepartmentGroup.name())
+                .isEqualTo("테스트");
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 학과 그룹 ID 로 조회하면 실패한다.")
+    void getDepartmentGroup_fail_noExistsId() {
+        //given
+        when(departmentGroupRepository.findById(any(Long.class)))
+                .thenReturn(Optional.empty());
+        //when, then
+        Assertions.assertThatThrownBy(() -> departmentGroupService.getDepartmentGroup(1L))
+                .isInstanceOf(NotFoundElementException.class);
+    }
+
+    @Test
+    @DisplayName("모든 학과그룹에 대한 조회 요청을 보내면 성공한다.")
+    void getDepartmentGroups_success() {
+        //given
+        var departmentGroup = new DepartmentGroup("1");
+        when(departmentGroupRepository.findAll())
+                .thenReturn(List.of(departmentGroup, departmentGroup, departmentGroup));
+        //when
+        var departmentGroups = departmentGroupService.getDepartmentGroups();
+        //then
+        Assertions.assertThat(departmentGroups.size())
+                .isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("모든 학과그룹을 조회하면 비어있어도 성공한다.")
+    void getDepartmentGroups_success_emptyList() {
+        //given
+        when(departmentGroupRepository.findAll())
+                .thenReturn(List.of());
+        //when
+        var departmentGroups = departmentGroupService.getDepartmentGroups();
+        //then
+        Assertions.assertThat(departmentGroups.size())
+                .isEqualTo(0);
     }
 
     private DepartmentGroupRequest getDepartmentGroupRequest() {
