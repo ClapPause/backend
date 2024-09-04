592f9a8732e575a9cac0469d203e962ef4d78b8c
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-09-05 01:21:39 +0900

    test: 학과그룹의 조회 요청에 대한 테스트코드 작성
    학과그룹의 조회요청에 대한 테스트코드 작성

--- src/main/java/com/clap/pause/repository/DepartmentGroupRepository.java
+++ src/main/java/com/clap/pause/repository/DepartmentGroupRepository.java
@@ -3,10 +3,6 @@ package com.clap.pause.repository;
 import com.clap.pause.model.DepartmentGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface DepartmentGroupRepository extends JpaRepository<DepartmentGroup, Long> {
-    Optional<DepartmentGroup> findByName(String name);
-
     boolean existsByName(String name);
 }
--- src/test/java/com/clap/pause/controller/DepartmentGroupControllerTest.java
+++ src/test/java/com/clap/pause/controller/DepartmentGroupControllerTest.java
@@ -28,6 +28,7 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -116,6 +117,57 @@ class DepartmentGroupControllerTest {
                 .updateDepartmentGroup(any(Long.class), any(DepartmentGroupRequest.class));
         //when
         var result = mockMvc.perform(put("/api/department-groups/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(departmentGroupRequest))
+                .with(csrf()));
+        //then
+        var postResult = result.andExpect(status().isNotFound())
+                .andReturn();
+        var response = getExceptionResponseMessage(postResult);
+
+        Assertions.assertThat(response.status())
+                .isEqualTo(HttpStatus.NOT_FOUND.value());
+        Assertions.assertThat(response.message())
+                .isEqualTo("1를 가진 학과그룹이 존재하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("존재하는 ID 로 조회 요청을 보내면 성공한다.")
+    void getDepartmentGroup_success() throws Exception {
+        //given
+        var departmentGroupRequest = new DepartmentGroupRequest("테스트");
+        var departmentGroupResponse = new DepartmentGroupResponse(1L, "테스트");
+
+        when(departmentGroupService.getDepartmentGroup(any(Long.class)))
+                .thenReturn(departmentGroupResponse);
+        //when
+        var result = mockMvc.perform(get("/api/department-groups/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(departmentGroupRequest))
+                .with(csrf()));
+        //then
+        var getResult = result.andExpect(status().isOk())
+                .andReturn();
+        var response = getResult.getResponse()
+                .getContentAsString();
+        var convertedDepartmentGroupResponse = objectMapper.readValue(response, DepartmentGroupResponse.class);
+
+        Assertions.assertThat(convertedDepartmentGroupResponse.id())
+                .isEqualTo(1L);
+        Assertions.assertThat(convertedDepartmentGroupResponse.name())
+                .isEqualTo("테스트");
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 ID 로 조회 요청을 보내면 성공한다.")
+    void getDepartmentGroup_fail_noExistsId() throws Exception {
+        //given
+        var departmentGroupRequest = new DepartmentGroupRequest("테스트");
+
+        doThrow(new NotFoundElementException("1를 가진 학과그룹이 존재하지 않습니다.")).when(departmentGroupService)
+                .getDepartmentGroup(any(Long.class));
+        //when
+        var result = mockMvc.perform(get("/api/department-groups/1")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(departmentGroupRequest))
                 .with(csrf()));
