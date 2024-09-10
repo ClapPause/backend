22c7eb9403e02220dbf66c86dc51d266d3d46a3f
boyun <bu339@naver.com>
2024-09-10 17:04:53 +0900

    fix : develop과 머지

--- src/main/java/com/clap/pause/controller/PostController.java
+++ src/main/java/com/clap/pause/controller/PostController.java
@@ -1,8 +1,9 @@
 package com.clap.pause.controller;
 
+import com.clap.pause.dto.post.request.ImageVoteRequest;
 import com.clap.pause.dto.post.request.PostRequest;
+import com.clap.pause.dto.post.request.TextVoteRequest;
 import com.clap.pause.dto.post.response.PostListResponse;
-import com.clap.pause.exception.PostAccessException;
 import com.clap.pause.service.PostService;
 import jakarta.validation.Valid;
 import java.net.URI;
@@ -29,7 +30,6 @@ public class PostController {
 
     @PostMapping
     public ResponseEntity<Void> saveDefaultPost(@PathVariable(name = "departmentgroupId") Long departmentgroupId, @Valid @RequestBody PostRequest postRequest, @RequestPart(value = "image", required = false) List<MultipartFile> imageFiles) {
-    public ResponseEntity<Void> saveDefaultPost(@PathVariable(name = "departmentgroupId") Long departmentgroupId, @Valid @RequestBody PostRequest postRequest, @RequestPart(value = "image", required = false) List<MultipartFile> imageFiles) {
         var memberId = getMemberId();
         var post = postService.saveDefaultPost(memberId, postRequest, departmentgroupId, imageFiles);
         return ResponseEntity.created(URI.create("/api/departmentgroups/" + departmentgroupId + "/posts" + post.id()))
@@ -39,42 +39,7 @@ public class PostController {
     @PostMapping("/textvote")
     public ResponseEntity<Void> saveTextVote(@PathVariable(name = "departmentgroupId") Long departmentgroupId, @Valid @RequestBody TextVoteRequest textVoteRequest, @RequestPart(value = "image", required = false) MultipartFile imageFile) {
         var memberId = getMemberId();
-        var post = postService.saveDefaultPost(memberId, postRequest, departmentgroupId, imageFiles);
-        return ResponseEntity.created(URI.create("/api/departmentgroups/" + departmentgroupId + "/posts" + post.id()))
-                .build();
-    }
-
-    @GetMapping
-    public ResponseEntity<List<PostListResponse>> getAllPosts(@PathVariable(name = "departmentgroupId") Long departmentGroupId) throws PostAccessException {
-        var postListRespons = postService.getAllPosts(departmentGroupId);
-        return ResponseEntity.ok().body(postListRespons);
-    }
-
-    @GetMapping("/{postId}")
-    public ResponseEntity<PostListResponse> getPost(@PathVariable(name = "departmentgroupId") Long departmentGroupId, @PathVariable(name = "postId") Long postId) throws PostAccessException {
-        var response = postService.getPostResponse(postId);
-        return ResponseEntity.ok().body(response);
-    }
-
-    @PutMapping("/{postId}")
-    public ResponseEntity<Void> updatePost(@PathVariable(name = "departmentgroupId") Long departmentGroupId, @PathVariable(name = "postId") Long postId, @Valid @RequestBody PostRequest postRequest) {
-        postService.updatePost(postId, postRequest);
-        return ResponseEntity.noContent().build();
-    }
-
-    @DeleteMapping("/{postId}")
-    public ResponseEntity<Void> deletePost(@PathVariable(name = "departmentgroupId") Long departmentGroupId, @PathVariable(name = "postId") Long postId) {
-        postService.deletePost(postId);
-        return ResponseEntity.noContent().build();
         var post = postService.saveTextVote(memberId, textVoteRequest, departmentgroupId, imageFile);
-        return ResponseEntity.created(URI.create("/api/departmentgroups/" + departmentgroupId + "/posts" + post.id()))
-                .build();
-    }
-
-    @PostMapping("/imageVote")
-    public ResponseEntity<Void> saveImageVote(@PathVariable(name = "departmentgroupId") Long departmentgroupId, @Valid @RequestBody ImageVoteRequest imageVoteRequest, @RequestPart(value = "image") List<MultipartFile> imageFiles) {
-        var memberId = getMemberId();
-        var post = postService.saveImageVote(memberId, imageVoteRequest, departmentgroupId, imageFiles);
         return ResponseEntity.created(URI.create("/api/departmentgroups/" + departmentgroupId + "/posts" + post.id()))
                 .build();
     }
@@ -97,6 +62,20 @@ public class PostController {
         return ResponseEntity.noContent().build();
     }
 
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<Void> deletePost(@PathVariable(name = "departmentgroupId") Long departmentGroupId, @PathVariable(name = "postId") Long postId) {
+        postService.deletePost(postId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/imageVote")
+    public ResponseEntity<Void> saveImageVote(@PathVariable(name = "departmentgroupId") Long departmentgroupId, @Valid @RequestBody ImageVoteRequest imageVoteRequest, @RequestPart(value = "image") List<MultipartFile> imageFiles) {
+        var memberId = getMemberId();
+        var post = postService.saveImageVote(memberId, imageVoteRequest, departmentgroupId, imageFiles);
+        return ResponseEntity.created(URI.create("/api/departmentgroups/" + departmentgroupId + "/posts" + post.id()))
+                .build();
+    }
+
     @PutMapping("/textvote/{postId}")
     public ResponseEntity<Void> updateTextVote(@PathVariable(name = "departmentgroupId") Long departmentGroupId, @PathVariable(name = "postId") Long postId, @Valid @RequestBody TextVoteRequest textVoteRequest) {
         postService.updateTextVote(postId, textVoteRequest);
@@ -106,12 +85,6 @@ public class PostController {
     @PutMapping("/imageVote/{postId}")
     public ResponseEntity<Void> updateImageVote(@PathVariable(name = "departmentgroupId") Long departmentGroupId, @PathVariable(name = "postId") Long postId, @Valid @RequestBody ImageVoteRequest imageVoteRequest, @RequestPart List<MultipartFile> imageFiles) {
         postService.updateImageVote(postId, imageVoteRequest, imageFiles);
-        return ResponseEntity.noContent().build();
-    }
-
-    @DeleteMapping("/{postId}")
-    public ResponseEntity<Void> deletePost(@PathVariable(name = "departmentgroupId") Long departmentGroupId, @PathVariable(name = "postId") Long postId) {
-        postService.deletePost(postId);
         return ResponseEntity.noContent().build();
     }
 
