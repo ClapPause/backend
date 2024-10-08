511f7650e4b1d21c22c11be138cdd2e177033389
boyun <bu339@naver.com>
2024-10-08 15:29:35 +0900

    add : 핫포스트 컨트롤러 추가

---
+++ src/main/java/com/clap/pause/controller/HotPostController.java
@@ -0,0 +1,11 @@
+package com.clap.pause.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/posts/hot")
+public class HotPostController {
+}
