c3fa0f4fbb19e9f06bcc2dd8468483e60e6b59ce
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-10-16 14:36:38 +0900

    chore: Notification 도메인 구현
    도메인 구현

--- src/main/java/com/clap/pause/model/Notification.java
+++ src/main/java/com/clap/pause/model/Notification.java
@@ -18,8 +18,17 @@ public class Notification extends BaseEntity {
     @JoinColumn(name = "member_id", referencedColumnName = "id")
     private Member member;
     @NotNull
-    @Column(name = "community")
-    private Boolean community = Boolean.TRUE;
+    @Column(name = "ballooning_time")
+    private Boolean ballooningTime = Boolean.TRUE;
+    @NotNull
+    @Column(name = "simulation")
+    private Boolean simulation = Boolean.TRUE;
+    @NotNull
+    @Column(name = "event")
+    private Boolean event = Boolean.TRUE;
+    @NotNull
+    @Column(name = "service_use")
+    private Boolean serviceUse = Boolean.TRUE;
     @NotNull
     @Column(name = "recommend_post")
     private Boolean recommendPost = Boolean.TRUE;
@@ -27,17 +36,11 @@ public class Notification extends BaseEntity {
     @Column(name = "new_comment")
     private Boolean newComment = Boolean.TRUE;
     @NotNull
-    @Column(name = "my_post_comment")
-    private Boolean myPostComment = Boolean.TRUE;
-    @NotNull
-    @Column(name = "my_post_comment_like")
-    private Boolean myPostCommentLike = Boolean.TRUE;
-    @NotNull
-    @Column(name = "my_post_scrap")
-    private Boolean myPostScrap = Boolean.TRUE;
-    @NotNull
     @Column(name = "selected_hot_post")
     private Boolean selectedHotPost = Boolean.TRUE;
+    @NotNull
+    @Column(name = "activity")
+    private Boolean activity = Boolean.TRUE;
 
 
     protected Notification() {
@@ -45,5 +48,55 @@ public class Notification extends BaseEntity {
 
     public Notification(Member member) {
         this.member = member;
+    }
+
+    public void updateAll(Boolean value) {
+        this.ballooningTime = value;
+        this.simulation = value;
+        this.event = value;
+        this.serviceUse = value;
+        this.recommendPost = value;
+        this.newComment = value;
+        this.selectedHotPost = value;
+        this.activity = value;
+    }
+
+    public void updateCommunity(Boolean value) {
+        this.recommendPost = value;
+        this.newComment = value;
+        this.selectedHotPost = value;
+        this.activity = value;
+    }
+
+    public void updateBallooningTime(Boolean ballooningTime) {
+        this.ballooningTime = ballooningTime;
+    }
+
+    public void updateSimulation(Boolean simulation) {
+        this.simulation = simulation;
+    }
+
+    public void updateEvent(Boolean event) {
+        this.event = event;
+    }
+
+    public void updateServiceUse(Boolean serviceUse) {
+        this.serviceUse = serviceUse;
+    }
+
+    public void updateRecommendPost(Boolean recommendPost) {
+        this.recommendPost = recommendPost;
+    }
+
+    public void updateNewComment(Boolean newComment) {
+        this.newComment = newComment;
+    }
+
+    public void updateActivity(Boolean activity) {
+        this.activity = activity;
+    }
+
+    public void updateSelectedHotPost(Boolean selectedHotPost) {
+        this.selectedHotPost = selectedHotPost;
     }
 }
